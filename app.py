b1ffed89ceaa67df84046619fd6a77f2c5e0e50b
jnpicao <52253611+jnpicao@users.noreply.github.com>
2020-01-06 11:30:58 +0000

    Add files via upload

--- app.py
+++ app.py
@@ -26,7 +26,24 @@ from playhouse.shortcuts import model_to_dict
 ########################################
 # Begin database stuff
 
-DB = SqliteDatabase('predictions.db')
+
+if 'DATABASE_URL' in os.environ:
+    db_url = os.environ['DATABASE_URL']
+    dbname = db_url.split('@')[1].split('/')[1]
+    user = db_url.split('@')[0].split(':')[1].lstrip('//')
+    password = db_url.split('@')[0].split(':')[2]
+    host = db_url.split('@')[1].split('/')[0].split(':')[0]
+    port = db_url.split('@')[1].split('/')[0].split(':')[1]
+    DB = PostgresqlDatabase(
+        dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port,
+    )
+else:
+    DB = SqliteDatabase('predictions.db')
+
 
 class Prediction(Model):
     observation_id = IntegerField(unique=True)
