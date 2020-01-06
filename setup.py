6ae212bbe74db4806fe0e7478c313e0445b3f870
Joao N. Picao <jnpicaooliveira@gmail.com>
2020-01-06 17:45:58 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from os import path
+
+
+setup(
+    name='my-custom-transformers',
+    version='0.0.1',
+    description='heroku model deploy',
+    url='https://nuna.biz',
+    author='Some data scientist',
+    author_email='nuna@nuna.biz',
+    packages=['custom_transformers'],
+    install_requires=[],
+)
